8ef91257e58e300a21f10b8840a2b416e2261819
NICEGUYSATHYA SATHYA <digitaltosathya@gmail.com>
2024-07-22 03:51:58 +0000

    2nd_Results_Proof

--- nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/deps/nexus_project-dfe870bf7842ff2a.d
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/deps/nexus_project-dfe870bf7842ff2a.d
@@ -1,5 +1,5 @@
-/workspace/Nexus-2.0-zkVM_2/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/deps/nexus_project-dfe870bf7842ff2a: src/main.rs
+/workspace/Nexus-2.0-zkVM_2_lav/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/deps/nexus_project-dfe870bf7842ff2a: src/main.rs
 
-/workspace/Nexus-2.0-zkVM_2/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/deps/nexus_project-dfe870bf7842ff2a.d: src/main.rs
+/workspace/Nexus-2.0-zkVM_2_lav/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/deps/nexus_project-dfe870bf7842ff2a.d: src/main.rs
 
 src/main.rs:
--- nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
@@ -1,1 +1,1 @@
-/workspace/Nexus-2.0-zkVM_2/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspace/Nexus-2.0-zkVM_2/nexus-project/src/main.rs
+/workspace/Nexus-2.0-zkVM_2_lav/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspace/Nexus-2.0-zkVM_2_lav/nexus-project/src/main.rs
